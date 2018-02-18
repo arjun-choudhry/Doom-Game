1706d33652ee60777290ddf0273c8deb5a636b41
arjun <arjun.94.choudhry@gmail.com>
2018-02-18 15:18:38 +0530

    implementing n-step Q learning

--- experience_replay.py
+++ experience_replay.py
@@ -17,3 +17,33 @@ class NStepProgress:
         self.env = env
         self.n_step = n_step
 
+    def __iter__(self):
+        state = self.env.reset()
+        history = deque()
+        reward = 0.0
+        while True:
+            action = self.ai(np.array([state]))[0][0]
+            next_state, r, is_done, _ = self.env.step(action)
+            reward += r
+            history.append(Step(state = state, action = action, reward = r, done = is_done))
+            while len(history) > self.n_step + 1:
+                history.popleft()
+            if len(history) == self.n_step + 1:
+                yield tuple(history)
+            state = next_state
+            if is_done:
+                if len(history) > self.n_step + 1:
+                    history.popleft()
+                while len(history) >= 1:
+                    yield tuple(history)
+                    history.popleft()
+                self.rewards.append(reward)
+                reward = 0.0
+                state = self.env.reset()
+                history.clear()
+
+    def rewards_steps(self):
+        rewards_steps = self.rewards
+        self.rewards = []
+        return rewards_steps
+
